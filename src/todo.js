e4db7c628e8610b22618c0af93c6f2791bd253fc
Joe McCann <joe.mccann@mailbox.org>
2023-01-15 03:24:31 -0500

    Move todo setter functions to separate declarations.

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,20 @@
+function setTitle(title) {
+  this.title = title;
+}
+
+function setDescription(description) {
+  this.description = description;
+}
+
+function setDueDate(dueDate) {
+  this.dueDate = dueDate;
+}
+
+function setPriority(priority) {
+  this.priority = priority;
+}
+
+
 export default function Todo(
   title,
   description = '',
@@ -10,17 +27,9 @@ export default function Todo(
     description,
     dueDate,
     priority,
-    setTitle: function (t) {
-      this.title = t;
-    },
-    setDescription: function(d) {
-      this.description = d;
-    },
-    setDueDate: function(dd) {
-      this.description = dd;
-    },
-    setPriority: function(p) {
-      this.priority = p;
-    }
+    setTitle,
+    setDescription,
+    setDueDate,
+    setPriority,
   }
 }
